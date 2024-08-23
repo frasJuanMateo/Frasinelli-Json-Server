ca00824a3c0d1729c8bed50193ba86f8d509f20c
Juan Mateo FRASINELLI <d46710280@alumnos.itr.edu.ar>
2024-08-23 12:37:30 -0300

    Se añadio logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import SearchBar from './SearchBar.jsx'
+import Logo from '../assets/hampter.webp'
 
 function NavBar({data}) {
     return (
         <>
+        <img src={Logo} style={{width:"200px", height:"100px", border: "2px solid #000000"}} />
         <SearchBar/>
         </>
     )
