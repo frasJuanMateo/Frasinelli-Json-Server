303df2d40b393b4ee598161ef470a53b1e77a2b5
Juan Mateo FRASINELLI <d46710280@alumnos.itr.edu.ar>
2024-08-28 11:25:59 -0300

    Se añadio la barra de busca

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react'
 
-function SearchBar({data}) {
+function SearchBar({ data }) {
     return (
         <>
+            <label>
+                Filtrar:<input style={{width:"min-content"}} name='search' type="text" />
+            </label>
         </>
     )
 }
